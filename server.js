d6332fe4bd6a379192738c236c059cb49a6c94e8
Fazbr <147539294+Fazbr@users.noreply.github.com>
2024-03-17 08:39:05 +0000

    14

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
